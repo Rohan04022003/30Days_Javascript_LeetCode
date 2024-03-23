fe6bfbb1fe0f8b617dd6ddc8843022cff393ba44
Rohan Kumar Mahto <131887978+Rohan04022003@users.noreply.github.com>
2024-03-23 15:23:20 +0530

    Time: 48 ms (78.77%), Space: 48.8 MB (54.95%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,41 @@
+function expect(val) {
+    return {
+        toBe: (otherVal) => {
+            if(val === otherVal){
+                return true
+            }
+            else{
+                throw new Error ("Not Equal")
+            }
+        },
+        notToBe: (otherVal) => {
+            if(val !== otherVal){
+                return true
+            }
+            else{
+                throw new Error ("Equal")
+            }
+        }
+    };
+}
+
+// Example usage:
+
+try{
+    console.log({"value": expect(5).toBe(5)})
+}
+catch(error){
+    console.log({"error": error.message});
+}
+
+try {
+    console.log({"error": expect(5).toBe(null)})
+} catch (error) {
+    console.log({"error": error.message});
+}
+
+try {
+    console.log({"value": expect(5).notToBe(null)})
+} catch (error) {
+    console.log({"error": error.message});
+}
