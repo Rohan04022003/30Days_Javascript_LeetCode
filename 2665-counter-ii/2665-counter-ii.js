d8fa9be70ad1c523861b9d56ed418c9cd1a77c86
Rohan Kumar Mahto <131887978+Rohan04022003@users.noreply.github.com>
2024-03-26 11:09:41 +0530

    Time: 54 ms (81.20%), Space: 51.5 MB (62.29%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -21,10 +21,12 @@ var createCounter = function(init) {
 //Example 1:
 
 const counter1 = createCounter(5);
-console.log([counter1.increment(), counter1.reset(), counter1.decrement()])
+let result1 = [counter1.increment(), counter1.reset(), counter1.decrement()]
+console.log(result1)
 
 //Example 2:
 
 const counter2 = createCounter(0);
-console.log([counter2.increment(), counter2.increment(), counter2.decrement(), counter2.reset(), counter2.reset()])
+let result2 = [counter2.increment(), counter2.increment(), counter2.decrement(), counter2.reset(), counter2.reset()]
+console.log(result2)
 
