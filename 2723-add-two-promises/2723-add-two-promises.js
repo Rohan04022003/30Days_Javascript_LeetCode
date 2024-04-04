d0461cc53260e2e1fd9d3962d96d086fb255671e
Rohan Kumar Mahto <131887978+Rohan04022003@users.noreply.github.com>
2024-04-04 23:27:26 +0530

    Time: 68 ms (28.17%), Space: 49.4 MB (71.87%) - LeetHub

---
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -0,0 +1,19 @@
+var addTwoPromises = async function(promise1, promise2) {
+  return Promise.all([promise1, promise2]).then(([num1, num2]) => num1 + num2)
+ };
+
+// Example 1
+const promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20));
+const promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60));
+
+addTwoPromises(promise1, promise2).then(result => {
+  console.log("Example 1:", result); // Output: 7
+});
+
+// Example 2
+const promise3 = new Promise(resolve => setTimeout(() => resolve(10), 50));
+const promise4 = new Promise(resolve => setTimeout(() => resolve(-12), 30));
+
+addTwoPromises(promise3, promise4).then(result => {
+  console.log("Example 2:", result); // Output: -2
+});
