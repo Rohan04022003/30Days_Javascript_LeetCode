78826da2be4fccd70698b157b99ad299bcd5fbb3
Rohan Kumar Mahto <131887978+Rohan04022003@users.noreply.github.com>
2024-04-05 23:06:06 +0530

    Time: 56 ms (34.51%), Space: 48.9 MB (38.77%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,12 @@
+
+let sleep = async function(millis) {
+    return new Promise(resolve =>{
+        setTimeout(resolve, millis)
+    })
+}
+
+ let t = Date.now()
+ sleep(100).then(() => console.log(Date.now() - t)) // 100
+
+let t2 = Date.now()
+sleep(200).then(() => console.log(Date.now() - t)) // 200
