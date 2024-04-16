8aa25f8c059cecd7a18dbc6f5891c984cfa0c268
Rohan Kumar Mahto <131887978+Rohan04022003@users.noreply.github.com>
2024-04-16 23:33:54 +0530

    Time: 94 ms (28.53%), Space: 62.1 MB (22.62%) - LeetHub

---
+++ 2705-compact-object/2705-compact-object.js
@@ -0,0 +1,24 @@
+function compactObject(obj) {
+    if (Array.isArray(obj)) {
+        return obj.filter(value => Boolean(value)).map(compactObject);
+    } else if (typeof obj === 'object' && obj !== null) {
+        const result = {};
+        Object.entries(obj).forEach(([key, value]) => {
+            const compactedValue = compactObject(value);
+            if (Boolean(compactedValue)) {
+                result[key] = compactedValue;
+            }
+        });
+        return result;
+    } else {
+        return obj;
+    }
+}
+const obj1 = [null, 0, false, 1];
+console.log(compactObject(obj1)); // Output: [1]
+
+const obj2 = {"a": null, "b": [false, 1]};
+console.log(compactObject(obj2)); // Output: {"b": [1]}
+
+const obj3 = [null, 0, 5, [0], [false, 16]];
+console.log(compactObject(obj3)); // Output: [5, [], [16]]
